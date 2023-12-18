5c3e82fdce1cc9aec4bcc772a0d35098d5f44421
Alexander Müller <nckgr1234@gmail.com>
2023-12-18 15:20:49 +0100

    Add modular dropdown, with effects

--- src/dropdown-menu/main.js
+++ src/dropdown-menu/main.js
@@ -3,6 +3,8 @@ class DropdownMenu {
     this._menu = menuNode;
     this._menuHeading = this._menu.querySelector(".dropdown-menu-title");
     this._menuItems = this._menu.querySelector(".dropdown-item-container");
+    // duplicated to prevent dropdownbeing visible when opening page
+    this._menuItems.classList.add("d-none-items");
     this.hideItems();
     this._menuHeading.addEventListener("click", () =>
       this.toggleItemsVisibility()
@@ -18,10 +20,12 @@ class DropdownMenu {
   hideItems() {
     this._showingItems = false;
     this._menuItems.classList.add("hide-items");
+    setTimeout(() => this._menuItems.classList.add("d-none-items"), 500);
   }
   showItems() {
     this._showingItems = true;
-    this._menuItems.classList.remove("hide-items");
+    this._menuItems.classList.remove("d-none-items");
+    setTimeout(() => this._menuItems.classList.remove("hide-items"), 1);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./style.css";
+import "./dropdown-menu/functionality.css";
+import "./dropdown-menu/styling.css";
 import initDropdownMenus from "./dropdown-menu/main";
 
 initDropdownMenus();
