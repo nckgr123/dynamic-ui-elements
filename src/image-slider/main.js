4c91638570de64fc045cf2466fad995f88546a60
Alexander Müller <nckgr1234@gmail.com>
2023-12-19 17:56:12 +0100

    Add active element switching for image slider

---
+++ src/image-slider/main.js
@@ -0,0 +1,56 @@
+class Slider {
+  constructor(sliderNode) {
+    this._showSlideForMS = 5000;
+    this._container = sliderNode;
+    this._container.classList.add("slider-initialized");
+    this._slides = this._container.querySelectorAll(".slide");
+    this._pagination = this._container.querySelector(".slider-pagination");
+    this._prev = this._container.querySelector(".slider-nav .prev");
+    this._next = this._container.querySelector(".slider-nav .next");
+    
+    this._prev.addEventListener('click',()=>{
+        this.prevSlide();
+        this.resetNextTimer()
+    })
+    this._next.addEventListener('click',()=>{
+        this.nextSlide();
+        this.resetNextTimer()
+    })
+
+    this._activeSlideIndex = 0;
+    this.activateSlide(this._activeSlideIndex);
+    this.resetNextTimer();
+  }
+  resetNextTimer() {
+    clearInterval(this._nextTimer);
+    this._nextTimer = setInterval(
+      this.nextSlide.bind(this),
+      this._showSlideForMS
+    );
+  }
+  nextSlide() {
+    this.deactivateSlide(this._activeSlideIndex);
+    this._activeSlideIndex = (this._activeSlideIndex + 1) % this._slides.length;
+    this.activateSlide(this._activeSlideIndex);
+  }
+  prevSlide() {
+    this.deactivateSlide(this._activeSlideIndex);
+    this._activeSlideIndex = (this._activeSlideIndex - 1 + this._slides.length) % this._slides.length;
+    this.activateSlide(this._activeSlideIndex);
+  }
+  activateSlide(slideIndex) {
+    let slideToActivate = this._slides[slideIndex];
+    slideToActivate.classList.add("active");
+  }
+  deactivateSlide(slideIndex) {
+    let slideToDeactivate = this._slides[slideIndex];
+    slideToDeactivate.classList.remove("active");
+  }
+}
+
+const initSliders = () => {
+  let allSliders = document.querySelectorAll(".slider-container");
+  allSliders.forEach((sliderNode) => new Slider(sliderNode));
+};
+
+export default initSliders;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import "./style.css";
 import "./dropdown-menu/functionality.css";
 import "./dropdown-menu/styling.css";
 import initDropdownMenus from "./dropdown-menu/main";
+import initSliders from "./image-slider/main";
 
 initDropdownMenus();
+initSliders();
